6a62fddc6293dbef13263b01272238fabf9d51ae
Matthew J Pearson <seaofcode@gmail.com>
2025-03-24 11:05:06 -0500

    Instantiate
    Particles on Destroy for Enemies

--- OnRailsShooter2025/Assets/Scripts/Enemy.cs
+++ OnRailsShooter2025/Assets/Scripts/Enemy.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destoryedVFX;
+
     void OnParticleCollision(GameObject other)
     {
+        Instantiate(destoryedVFX, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
