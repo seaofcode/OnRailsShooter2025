997512a208ada4e7ba543969438677c646486d16
Matthew J Pearson <seaofcode@gmail.com>
2025-03-25 10:22:20 -0500

    Score Board UI
    Displaying Score on UI Canvas, imported Text mesh pro

--- OnRailsShooter2025/Assets/Scripts/Scoreboard.cs
+++ OnRailsShooter2025/Assets/Scripts/Scoreboard.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class Scoreboard : MonoBehaviour
 {
+    [SerializeField] TMP_Text scoreboardText;
+
     int score = 0;
 
     public void IncreaseScore(int amount)
     {
         score += amount;
-        Debug.Log(score);
+        scoreboardText.text = score.ToString();
     }
 }
