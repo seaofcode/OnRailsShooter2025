2fc771b61528aa7a99712e2d24a721481f11cec8
Matthew J Pearson <seaofcode@gmail.com>
2025-03-25 09:12:56 -0500

    Enemy Hit Points
    Enemy Hit Points, Dividing Enemy Class into Single Responsibility scripts, created Scoreboard script,

--- OnRailsShooter2025/Assets/Scripts/Enemy.cs
+++ OnRailsShooter2025/Assets/Scripts/Enemy.cs
@@ -3,10 +3,30 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destoryedVFX;
+    [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
+
+    Scoreboard scoreboard;
+
+    void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
 
     void OnParticleCollision(GameObject other)
     {
-        Instantiate(destoryedVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+
+        if (hitPoints <= 0)
+        {
+            scoreboard.IncreaseScore(scoreValue);
+            Instantiate(destoryedVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 }
